522e1a6a5cfaff5bdbe9e4a5c360c6f5ea6243fb
rs-rocha <robsonrocha.unb@gmail.com>
2026-08-10 20:35:53 -0300

    Implementa a entidade AlunoEntity

--- src/main/java/com/example/projetospringbootcommysql/entity/AlunoEntity.java
+++ src/main/java/com/example/projetospringbootcommysql/entity/AlunoEntity.java
@@ -1,4 +1,81 @@
 package com.example.projetospringbootcommysql.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class AlunoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private int idade;
+    private String registroAluno;
+    private String emailInstitucional;
+    private String turma;
+    private String periodo;
+
+    public String apresentacao(){
+        return "Meu nome é " + getNome() + ", " +
+                "entrei no curso porque quero ser programador e " +
+                "estou fazendo meu trabalho de Spring Boot."
+                ;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRegistroAluno() {
+        return registroAluno;
+    }
+
+    public void setRegistroAluno(String registroAluno) {
+        this.registroAluno = registroAluno;
+    }
+
+    public String getEmailInstitucional() {
+        return emailInstitucional;
+    }
+
+    public void setEmailInstitucional(String emailInstitucional) {
+        this.emailInstitucional = emailInstitucional;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
 }
--- src/main/java/com/example/projetospringbootcommysql/repository/AlunoRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.projetospringbootcommysql.repository;
-
-public class AlunoRepository {
-}
