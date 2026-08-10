abd739151f8ade5535a9083d5569d2d945dec859
rs-rocha <robsonrocha.unb@gmail.com>
2026-08-10 20:44:59 -0300

    Implementa AlunoController

--- src/main/java/com/example/projetospringbootcommysql/controller/AlunoController.java
+++ src/main/java/com/example/projetospringbootcommysql/controller/AlunoController.java
@@ -1,4 +1,26 @@
 package com.example.projetospringbootcommysql.controller;
 
+import com.example.projetospringbootcommysql.entity.AlunoEntity;
+import com.example.projetospringbootcommysql.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+@CrossOrigin(origins = "*")
 public class AlunoController {
+
+    @Autowired
+    private AlunoRepository repository;
+
+    @PostMapping
+    public AlunoEntity salvaAlunoEntity(@RequestBody AlunoEntity aluno){
+        return repository.save(aluno);
+    }
+    @GetMapping
+    public List<AlunoEntity> listarAlunos(){
+        return repository.findAll();
+    }
 }
